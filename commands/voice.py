332825463d3acf8b4308fb0935886d7924e52eed
Ankit_Anmol <87054411+Quantum-Codes@users.noreply.github.com>
2022-11-14 15:20:42 +0000

    Not sworking. Save before copy paste from stack overflow

--- commands/voice.py
+++ commands/voice.py
@@ -1,6 +1,20 @@
-import discord
+import discord, youtube_dl
 
 #https://stackoverflow.com/questions/60745020/is-there-a-way-to-directly-stream-audio-from-a-youtube-video-using-youtube-dl-or EPIC ANSWER
+
+def get_video(url):
+  ydl_opts = {'format': 'bestaudio', 'noplaylist': 'True'}
+  with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+     song_info = ydl.extract_info(url, download=False)
+
+  keys = ["title", "thumbnail", "description", "upload_date", "view_count", "webpage_url"]
+  video = {}
+  for item in keys:
+    video[item] = song_info[item]
+
+  video["stream_url"] = song_info["formats"][0]["url"]
+  return video
+
 
 class voice(discord.Cog):
   guild_ids = [871696913987162112]
@@ -27,6 +41,14 @@ class voice(discord.Cog):
     else:
       await ctx.respond("I'm not in a voice channel, use the /join command to make me join")
 
+  @discord.slash_command(name="play", description = "play a YouTube song with url (later search will exist)", guild_ids=guild_ids)
+  async def play(self, ctx, url:str):
+    if ctx.voice_client:
+      music = get_video(url)
+      FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}#no idea what's this
+      ctx.voice_client.play(discord.FFmpegPCMAudio(music["stream_url"], **FFMPEG_OPTIONS)) #double asterisk why
+    else:
+      await ctx.respond("Join a voice channel, then use /join andthen use this command to play. Later version: I will join automatically when /play")
 
 
 
